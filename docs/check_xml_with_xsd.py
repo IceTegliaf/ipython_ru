51e498a1983a53f97ecd0bf00f388a61760e6cac
Kovalenko Pavel <pavel@bitrain.ru>
2011-10-20 01:31:11 +0400

    first valid: ipython_dox.xml, ipython_dox.xsd

--- docs/check_xml_with_xsd.py
+++ docs/check_xml_with_xsd.py
@@ -2,10 +2,10 @@
 from lxml import etree
 
 
-xmlschema_doc = etree.parse("dox.xsd")
+xmlschema_doc = etree.parse("ipython_dox.xsd")
 xmlschema = etree.XMLSchema(xmlschema_doc)
 
-f = open("example_dox.xml")
+f = open("ipython_dox.xml")
 doc = etree.parse(f)
 
 print xmlschema.assertValid(doc)
