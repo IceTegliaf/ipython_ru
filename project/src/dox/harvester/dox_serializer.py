20cdb197e4436b838368731c56b37ebaf1d9e9ac
Kovalenko Pavel <pavel@bitrain.ru>
2011-10-21 04:54:03 +0400

    xml-dox without subclasses

--- project/src/dox/harvester/dox_serializer.py
+++ project/src/dox/harvester/dox_serializer.py
@@ -1,29 +1,61 @@
+import urllib
+from django.utils.encoding import force_unicode
+import types
 
 class Node(object):
+    ESCAPE_VALUE = (("&", "&amp;"), ("<","&lt;"), (">","&gt;"), ('\\','\\\\'), ('"', '\\"'))
+    ESCAPE_DATA = ((u"\x04", "\\x04"), )
     
     def __init__(self, name):
         self.name = name
-        self.attrs={}        
+        self.attrs=[]        
         self.childs = []
-        self.data
+        self._data = None
         
     def set(self, name, value):
-        self.attrs[name] = unicode(value)
+        if isinstance(value, types.BooleanType):
+            value = "True" if value else "False"
+        
+        self.attrs.append((name,unicode(value)))
         
     def data(self, data):
-        self.data = data
+        if isinstance(data, types.BooleanType):
+            self._data = "True" if data else "False"
+        else:
+            self._data = data
+        
+    
+    @staticmethod
+    def escape_value(value):
+        #TODO: may be regex faste?
+        for v in Node.ESCAPE_VALUE:
+            value = value.replace(*v)
+        return value 
+#        return urllib.quote(value, " ")
+
+    @staticmethod
+    def  escape_data(data):
+        for v in Node.ESCAPE_DATA:
+            data = data.replace(*v)
+        return data 
+ 
         
         
     def to_string(self):
         out=["<%s" % self.name]
         if self.attrs:
             out.append(" ")
-            out.append(" ".join([ '%s="%s"' % (name, value.replace('"','\\"')) for name,value in self.attrs.items() ]))
+            out.append(" ".join([ '%s="%s"' % (name, Node.escape_value(value)) for name,value in self.attrs ]))
         out.append(">")
+        
+        if self._data:
+            out.append("<![CDATA[")
+            out.append(Node.escape_data(force_unicode(self._data)))        
+            out.append("]]>")
 
         out+=[child.to_string() for child in self.childs]
         
-        out.append("</%s>" % self.name)
+        out.append("</%s>\n" % self.name)
         return "".join(out)
 
 
@@ -40,7 +72,7 @@ class Serialize(object):
             self.last.childs.append(node)
         self.last = node
         self.stack.append(node)
-        return node                
+        return self                
         
     def end(self):
         self.stack.pop()
@@ -51,24 +83,109 @@ class Serialize(object):
             
     def set(self, name, value, skip_none = False):
         if skip_none and not value:
-            return
+            return self
         self.last.set(name, value)
+        return self
 
-    def data(self, name, value):
-        self.last.data(name, value)
+    def data(self, value):
+        self.last.data(value)
+        return self
+        
+        
+    def add_object(self, vars, functions, classes):
+        for var in vars:
+            if not var.is_local():
+                self.begin("import_attribute")\
+                    .set("name", var.name)\
+                    .set("type", var.type_name)\
+                    .set("module", var.source_module())\
+                    .end()
+
+        for func in functions:
+            if not func.is_local():                
+                self.begin("import_function")\
+                    .set("name", func.name)\
+                    .set("module", func.source_module())\
+                    .end()   
+                                
+        for cls in classes:
+            if not cls.is_local():                
+                self.begin("import_class").set("name", cls.name).set("module", cls.source_module())
+                self.end()
+                
+             
+
+        for var in vars:
+            if var.is_local():
+                self.begin("attribute").set("name", var.name).set("type", var.type_name)
+                
+                self.begin("value").data(var.value).end()
+                self.end()
+
+
+        for func in functions:
+            if func.is_local():
+                self.begin("function").set("name", func.name)
+                spec = func.argspec
+                print "func:", func.name, spec
+                self.set("args", spec.varargs, skip_none = True)
+                self.set("kwargs", spec.keywords, skip_none = True)
+                
+                #TODO: check type="static"                 
+                self.begin("doc").data(func.get_doc()).end()
+                
+                num=1
+                for name in spec.args:
+                    self.begin("argument").set("name", name)
+                    pos = len(spec.args) - num
+                    if spec.defaults and pos<len(spec.defaults):
+                        value = spec.defaults[len(spec.defaults)-1-pos]
+                        self.set("type", type(value).__name__)
+                        
+                        self.begin("value").data(value).end()
+#                        self.set("value", value)
+                    self.end()
+                    num+=1
+                
+                                
+                self.end()
+                                
+        for cls in classes:
+            if cls.is_local():
+                self.begin("class").set("name", cls.name).end()
+                
+                
+
         
         
     def to_string(self):
-        root = self.begin("dox:ipython_ru")
-        self.set("xsi:schemaLocation", "dox.xsd")
-        self.set("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance")
-        self.set("xmlns:dox", "http://ipython.ru/dox/v1")
+        self.begin("dox:ipython_ru")\
+            .set("xsi:schemaLocation", "dox.xsd")\
+            .set("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance")\
+            .set("xmlns:dox", "http://ipython.ru/dox/v1")
+            
+        root = self.last
         
         for pkg in self.packages:
-            self.begin("package")
-            self.set("name", pkg.name)
-            self.set("version", pkg.version, skip_none = True)
-            self.set("file", pkg.file)
+            self.begin("package")\
+                .set("name", pkg.name)\
+                .set("version", pkg.version, skip_none = True)\
+                .set("file", pkg.file)
+            
+            if pkg.import_error:
+                self.set("error", pkg.import_error)
+            else:
+                #add doc
+                self.begin("doc").data(pkg.get_doc()).end()
+                
+                #depends
+                for depends in pkg.depending_on:
+                    self.begin("depends").set("name", depends).end()
+                
+                #add attributes + function + classes
+                self.add_object(pkg.vars, pkg.functions, pkg.classes)
+                
+            
             self.end()
         
         self.end()
--- project/src/dox/harvester/spider.py
+++ project/src/dox/harvester/spider.py
@@ -3,6 +3,7 @@ import types
 from tools.class_loader import get_class_by_string, extract_module_name
 import os
 from dox.harvester.dox_serializer import Serialize
+from inspect import isclass, getargspec
 
 PYTHON_EXT = [".py", ".pyc", ".pyo"]
 
@@ -12,8 +13,33 @@ class Variable(object):
         self.pkg = pkg
         self.name = name
     
+    def get_doc(self):
+        return self.value.__doc__
+        
+    doc = property(get_doc)
     value = property(lambda self: getattr(self.pkg.mod, self.name))
     type = property(lambda self: type(self.value))
+    
+    type_name = property(lambda self: type(self.value).__name__)
+    
+#    def value_is_object(self):
+#        return isinstance(self.value, types.ObjectType)
+
+    def is_local(self):
+        if hasattr(self.value, "__module__"):
+            return self.value.__module__==self.pkg.name
+        return True
+    
+    def source_module(self):
+        if hasattr(self.value, "__module__"):
+            return self.value.__module__
+        return None
+        
+        
+class Function(Variable):    
+    
+    argspec = property(lambda self: getargspec(self.value))
+
 
 
 class VariableContainer(object):
@@ -25,27 +51,42 @@ class VariableContainer(object):
         for var in self.vars:
             if var.name==name:
                 return var
-        return None 
+        return None
+    
+class ClassContainer():
+    
+    def __init__(self):
+        self.classes = []
+        
+class FunctionContainer():
+    
+    def __init__(self):
+        self.functions = []        
             
+class Class(Variable, VariableContainer, FunctionContainer):
+    pass
+
+        
+#    def is_ref(self):
+#        return self.value.__module__!=self.pkg.name
     
-class Class(Variable, VariableContainer):
-    
-    def is_ref(self):
-        return self.value.__module__!=self.pkg.name
     
     
-    
-class Package(VariableContainer):
+class Package(VariableContainer, ClassContainer, FunctionContainer):
     VERSION_VARIANTS = ['__version__', 'version', 'VERSION']
+    SKIP_VARS = ['__builtins__', '__name__', '__doc__', '__path__', '__package__']
     
     
     def __init__(self, spider, name):
         VariableContainer.__init__(self)
+        ClassContainer.__init__(self)
+        FunctionContainer.__init__(self)
         
         self.spider = spider
         self.name = name
         self.file = ''
-        self.classes = []
+        
+        self.depending_on=[]
         self.import_error = None
         
     def get_version(self):
@@ -53,15 +94,22 @@ class Package(VariableContainer):
             version = self.find_var(variant)
             if version:
                 return version.value
-
         return ""
     version = property(get_version)
+    
+    def get_doc(self):
+        return self.mod.__doc__
+    
+    def add_depending_on(self, name):
+        if name not in self.depending_on:
+            self.depending_on.append(name)
+            
+    
     
 
         
     def harvest(self):
-        print "Run harvest for '%s'" % self.name
-
+#        print "Run harvest for '%s'" % self.name
         names = self.name.split(".")
         try:
             self.mod = __import__(self.name, {}, {}, [names[-1]])
@@ -74,20 +122,33 @@ class Package(VariableContainer):
             if isinstance(value, types.ModuleType):
                 self.spider.harvest_package(value.__name__)
                 continue
-            
-            if isinstance(value, types.ClassType):
+
+            #class
+            if isclass(value):
                 cls = Class(self, name)                
                 self.classes.append(cls)
-                
-                self.spider.harvest_package(cls.value.__module__)
+                if not cls.is_local():                
+                    self.spider.harvest_package(cls.source_module())
+                continue
+            
+            #function
+            if isinstance(value, types.FunctionType):
+                func = Function(self, name)
+                self.functions.append(func)
+                if not func.is_local():                
+                    self.spider.harvest_package(func.source_module())                
                 continue
             
             #parse local vars
             if name == "__file__":
                 self.file = self.spider.normalize_file_name(value)
                 
-            else:
-                self.vars.append(Variable(self, name))
+            elif name not in Package.SKIP_VARS:
+                v = Variable(self, name)
+                
+                if not v.is_local():
+                    self.spider.harvest_package(v.source_module())
+                self.vars.append(v)
                 
         #scan files
         if os.path.basename(self.mod.__file__).startswith("__init__."): #scan all files
@@ -128,15 +189,20 @@ class Spider(object):
         self.harvested = []
         self.packages = []
         self.depending_on=[]
-        
+        self.current_pkg = None        
         self.base = package_name
-                
+                        
         self.harvest_package(self.base)        
         
         
         
     def harvest_package(self, name):
+        if not name:
+            return
         if not name.startswith(self.base):
+            
+            self.current_pkg.add_depending_on(name)
+            
             if name not in self.depending_on: 
                 self.depending_on.append(name)
                 
@@ -145,10 +211,10 @@ class Spider(object):
         
     def harvest(self):
         while self.to_harvest:
-            pkg = Package(self, self.to_harvest.pop())
-            self.harvested.append(pkg.name)
-            pkg.harvest()
-            self.packages.append(pkg)
+            self.current_pkg = Package(self, self.to_harvest.pop())
+            self.harvested.append(self.current_pkg.name)
+            self.current_pkg.harvest()
+            self.packages.append(self.current_pkg)
             
     def normalize_file_name(self, file_name):
         result = file_name
